1b8f96f1d105b7c77b126531bfa5ce5b7968766a
akram-hossain <nayanbd17@gmail.com>
2018-11-22 19:07:33 +0600

    finance psd

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(function(){
+    //Slick slider part start 
+      $('.slider_ban').slick({
+          autoplay: true,
+          arrows: false,
+          dots: false,
+      });
+    // Slick slider part end
+});
